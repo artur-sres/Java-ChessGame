49b0596fa28a23ab6d0416dca43a226b396208f9
Artur <asr.engsoft@gmail.com>
2025-09-08 14:30:22 -0300

    Update Rook.java

--- chess/pieces/Rook.java
+++ chess/pieces/Rook.java
@@ -4,7 +4,7 @@ import boardgame.Board;
 import boardgame.Color;
 import chess.ChessPiece;
 
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
     public Rook(Board board, Color color) {
         super(board, color);
     }
